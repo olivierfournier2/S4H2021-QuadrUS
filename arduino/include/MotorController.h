b55e0f8aad8d63234311a929d5e60ad9bd4eff51
Anthony Breton <anthonybreton@outlook.com>
2021-03-24 13:24:26 -0400

    Ajustment low level motor control

--- arduino/include/MotorController.h
+++ arduino/include/MotorController.h
@@ -9,33 +9,16 @@
 #include <std_msgs/MultiArrayDimension.h>
 #include <std_msgs/Float64MultiArray.h>
 
-
-//Node config
-ros::NodeHandle node_handle;
-std_msgs::Float64MultiArray motor_msg;
-
-//Function prototypes
-Adafruit_PWMServoDriver driver = Adafruit_PWMServoDriver();
-int angleToPulse(float ang);
-int radToPulse(float radAng);
-void motorControl(int pulseCommand[12]); //Function that controls the motors with an internal loop
-void speedSelection(int desiredSpeed);
-void subscriberCallback(const std_msgs::Float64MultiArray& motor_msg); //Function called with each SpinOnce()
-void motorController(int pulseCommand[12]); //Function that controls the motors without an internal loop
-
-//Setup the topic the node is subscribed to
-ros::Subscriber<std_msgs::Float64MultiArray> motor_subscriber("hw_cmd", &subscriberCallback);
-
-//Global variables
-
-int compensationPulse[12];
-
 #define PI 3.1415926535897932384626433832795
-
 #define PULSEMIN 555
 #define PULSEMAX 2395
-
 #define SERVO_FREQ 60
+
+//Global variables
+float initPositions[] = {0,90,0,
+                        0,90,0,
+                        0,90,0,
+                        0,90,0};
 
 float compensationArrayMec[12] = { 0.0,  -1.0,  -7.0,
                                    7.5,   7.0,  -7.0,
@@ -45,121 +28,88 @@ float compensationArrayMec[12] = { 0.0,  -1.0,  -7.0,
 
 float compensationArrayROS[12] = { 135.0,  225.0,  135.0,
                                    135.0,   45.0,  135.0,
-                                   135.0,   225.0,   135.0,
-                                   135.0,  45.0,  135.0}; 
+                                   135.0,  225.0,  135.0,
+                                   135.0,   45.0,  135.0}; 
 
-float compensationArray[12];
-                                                          
+int jointLimit[12][2] = { {-30, 45}, {-110, 125}, {-75, 135},
+                          {-45, 30}, {-125, 110}, {-135, 75},
+                          {-30, 45}, {-110, 125}, {-75, 135},
+                          {-45, 30}, {-125, 110}, {-135, 75} };
 
-//  MX: hanche/femur/tibia - left/right - front/rear    1475 pulses = 135 degrés
-//Moteurs :             M0:H-L-F  M1:F-L-F  M2:T-L-F  M3:H-L-R  M4:F-L-R  M5:T-L-R  M6:H-R-F  M7:F-R-F  M8:T-R-F  M9:H-R-R  M10:F-R-R  M11:T-R-R
-int currentPulse[12] = {1475,     1475,     1475,     1475,     1475,     1475,     1475,     1475,     1475,     1475,     1475,      1475};   // Position initiale en pulse
 
-//Moteurs :                        M0                    M1                    M2         
-int jointLimit[12][2] = { {angleToPulse(135 + compensationArrayMec[0] - 30), angleToPulse(135 + compensationArrayMec[0] + 45)}, {angleToPulse(135 + compensationArrayMec[1] - 110), angleToPulse(135 + compensationArrayMec[1] + 125)}, {angleToPulse(135 + compensationArrayMec[2] - 75), angleToPulse(135 + compensationArrayMec[2] + 135)},
-                          {angleToPulse(135 + compensationArrayMec[3] - 45), angleToPulse(135 + compensationArrayMec[3] + 30)}, {angleToPulse(135 + compensationArrayMec[4] - 125), angleToPulse(135 + compensationArrayMec[4] + 110)}, {angleToPulse(135 + compensationArrayMec[5] - 135), angleToPulse(135 + compensationArrayMec[5] + 75)},
-                          {angleToPulse(135 + compensationArrayMec[6] - 30), angleToPulse(135 + compensationArrayMec[6] + 45)}, {angleToPulse(135 + compensationArrayMec[7] - 110), angleToPulse(135 + compensationArrayMec[7] + 125)}, {angleToPulse(135 + compensationArrayMec[8] - 75), angleToPulse(135 + compensationArrayMec[8] + 135)},
-                          {angleToPulse(135 + compensationArrayMec[9] - 45), angleToPulse(135 + compensationArrayMec[9] + 30)}, {angleToPulse(135 + compensationArrayMec[10] - 125), angleToPulse(135 + compensationArrayMec[10] + 110)}, {angleToPulse(135 + compensationArrayMec[11] - 135), angleToPulse(135 + compensationArrayMec[11] + 75)} };    // En pulse
+//Function prototypes
+Adafruit_PWMServoDriver driver = Adafruit_PWMServoDriver();
+void computeLimits();
+int degToPulse(float ang);
+float radToDeg(float angleRad);
+void subscriberCallback(const std_msgs::Float64MultiArray& motor_msg);
+void moveMotor(float cmdAngle[12]);
+float compensateCommand(float rawAngle, int index);
+void motorController(int pulseCommand[12]);
 
-int pulseInterval = 1;
+//ROS config
+ros::NodeHandle node_handle;
+std_msgs::Float64MultiArray motor_msg;
+ros::Subscriber<std_msgs::Float64MultiArray> motor_subscriber("hw_cmd", &subscriberCallback);
 
+void computeLimits() {
+  for (int i = 0; i < 12; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+      jointLimit[i][j] =  degToPulse(135 + compensationArrayMec[i] + jointLimit[i][j]);
+    }
+    
+  }
+  
+}
 
 //Transform degree angle to pulse
-int angleToPulse(float ang) {
+int degToPulse(float ang) {
   int pulse = map(ang, 0, 270, PULSEMIN, PULSEMAX);
   return pulse;
 }
 
-//Transform rad angle to pulse
-int radToPulse(float radAng) {
-  int pulse = map(radAng*100,0 , (3 * PI / 2)*100, PULSEMIN, PULSEMAX);
-  return pulse;
+float radToDeg(float angleRad) {
+  return map(angleRad, 0, (2*PI), 0, 360);
 }
 
-// Select the speed of the motors (1 : slowest / 5 : fastest)
-// All motor will have the same speed
-void speedSelection(int desiredSpeed) {
-    switch (desiredSpeed) {
-    case 1 : 
-      pulseInterval = 1;
-      break;
-      
-    case 2 : 
-      pulseInterval = 5;
-      break;
+/**
+ * Receive angle commands from ROS and convert them to degrees
+ *
+ * @param motor_msg ROS controller message specifying joint angle in radian
+ */
+void subscriberCallback(const std_msgs::Float64MultiArray& motor_msg) {
 
-    case 3 : 
-      pulseInterval = 10;
-      break;
-
-    case 4 : 
-      pulseInterval = 15;
-      break;
-
-    case 5 : 
-      pulseInterval = 20;
-      break;
-      
-    
-    }
+  float ros_motor_commands[12];
+  
+  for (int i = 0; i < 12; i++)
+  {
+    ros_motor_commands[i] = radToDeg(motor_msg.data[i]);
   }
+  
+  moveMotor(ros_motor_commands);
+}
 
 
-// Param pulseCommand must be an array of pulse not angle
-// Use angleToPulse() function to populate array
-void motorControl(int pulseCommand[12]) {
-    bool stop = false;
-    int lastStep = 0;
+void moveMotor(float cmdAngle[12]) {
+  float compensatedCmd[12];
+  int pulseCmd[12];
 
-    for (int i = 0; i < 12; i++){
-        if(pulseCommand[i] > jointLimit[i][1]){
-            pulseCommand[i] = jointLimit[i][1];
-          }
-        else if(pulseCommand[i] < jointLimit[i][0]){
-            pulseCommand[i] = jointLimit[i][0];
-          }
-      }
-    
-    while(stop == false) {
-        stop = true;
-
-        for (int i = 0; i < 12 ; i++){
-          if (pulseCommand[i] > currentPulse[i]){
-              lastStep = pulseCommand[i] - currentPulse[i];
-              
-              if ( lastStep < pulseInterval) {
-                currentPulse[i] += lastStep;
-              } 
-              else {
-                currentPulse[i] += pulseInterval;
-              }
-                
-              driver.writeMicroseconds(i,currentPulse[i]);
-            }
-          else if (pulseCommand[i] < currentPulse[i]){
-              lastStep = currentPulse[i] - pulseCommand[i];
-              
-              if ( lastStep < pulseInterval) {
-                currentPulse[i] -= lastStep;
-              } 
-              else {
-                currentPulse[i] -= pulseInterval;
-              }
-              
-              driver.writeMicroseconds(i,currentPulse[i]);
-            }
-          else {
-              delayMicroseconds(1000);
-            }
-
-          if (currentPulse[i] != pulseCommand[i]) {
-              stop = false;
-            }
-        }
-      } 
+  for (int i = 0; i < 12; i++) {
+    compensatedCmd[i] = compensateCommand(cmdAngle[i], i);
+    pulseCmd[i] = degToPulse(compensatedCmd[i]);
   }
+  motorController(pulseCmd);
+}
 
-void motorController(int pulseCommand[12]){
+
+float compensateCommand(float rawAngle, int index) {
+  return rawAngle + compensationArrayMec[index] + compensationArrayROS[index];
+}
+
+
+void motorController(int pulseCommand[12]) {
 
     for (int i = 0; i < 12; i++){
         if(pulseCommand[i] > jointLimit[i][1]){
@@ -173,24 +123,6 @@ void motorController(int pulseCommand[12]){
     for (int i=0; i < 12; i++){
         driver.writeMicroseconds(i, pulseCommand[i]);
     }
-}
-
-
-void subscriberCallback(const std_msgs::Float64MultiArray& motor_msg) {
-
-  int command[12];
-  float ros_motor_commands[12] = {motor_msg.data[0], motor_msg.data[1], motor_msg.data[2], motor_msg.data[3], motor_msg.data[4], 
-  motor_msg.data[5], motor_msg.data[6], motor_msg.data[7], motor_msg.data[8], motor_msg.data[9], motor_msg.data[10], motor_msg.data[11]};
-  
-  float compensated_command[12];
-
-  for(int i = 0; i < 12; i++){
-    compensated_command[i] = compensationArray[i] + ros_motor_commands[i];
-    command[i] = radToPulse(compensated_command[i]);
-  };
-  
-  
-  motorController(command);
 }
 
 #endif
