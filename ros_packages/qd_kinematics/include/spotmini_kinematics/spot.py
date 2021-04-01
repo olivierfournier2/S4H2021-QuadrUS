726a89401c50243f145bd9a1a2eee8098367db38
Oreste <marj3220@usherbrooke.ca>
2021-04-01 09:58:29 -0400

    Changed to load the right URDF file

--- ros_packages/qd_kinematics/include/spotmini_kinematics/spot.py
+++ ros_packages/qd_kinematics/include/spotmini_kinematics/spot.py
@@ -381,13 +381,13 @@ class Spot(object):
         if reload_urdf:
             if self._self_collision_enabled:
                 self.quadruped = self._pybullet_client.loadURDF(
-                    pybullet_data.getDataPath() + "/assets/urdf/spot.urdf",
+                    pybullet_data.getDataPath() + "/../../../../../qd_simulation/robot_description/quadrus.urdf",
                     init_position,
                     useFixedBase=self._on_rack,
                     flags=self._pybullet_client.URDF_USE_SELF_COLLISION_EXCLUDE_PARENT)
             else:
                 self.quadruped = self._pybullet_client.loadURDF(
-                    pybullet_data.getDataPath() + "/assets/urdf/spot.urdf",
+                    pybullet_data.getDataPath() + "/../../../../../qd_simulation/robot_description/quadrus.urdf",
                     init_position,
                     INIT_ORIENTATION,
                     useFixedBase=self._on_rack)
