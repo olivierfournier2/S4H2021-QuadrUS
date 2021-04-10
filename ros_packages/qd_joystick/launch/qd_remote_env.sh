53094c1986922e8c0cbf138c6e81c6030fbd4d32
olivierfournier2 <olivier.fournier@usherbrooke.ca>
2021-04-10 17:18:15 -0400

    remote launch params

--- ros_packages/qd_joystick/launch/qd_remote_env.sh
+++ ros_packages/qd_joystick/launch/qd_remote_env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export ROS_MASTER_URI=http://ubuntudev-XPS-15-9550:11311/
-export ROS_HOSTNAME=pi_quadrus
+export ROS_MASTER_URI=http://192.168.4.17:11311/
+export ROS_IP="192.168.4.17"
+export ROSLAUNCH_SSH_UNKNOWN=1
 
 source /opt/ros/noetic/setup.bash
 source ~/quadrus_ws/devel/setup.bash
