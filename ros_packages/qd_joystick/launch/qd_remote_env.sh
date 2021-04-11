13fb56c7c2b43e77e67504eac8e31e15ef2995b1
olivierfournier2 <olivier.fournier@usherbrooke.ca>
2021-04-11 13:09:29 -0400

    remote launch

--- ros_packages/qd_joystick/launch/qd_remote_env.sh
+++ ros_packages/qd_joystick/launch/qd_remote_env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export ROS_MASTER_URI=http://192.168.4.17:11311/
-export ROS_IP="192.168.4.17"
+export ROS_IP="192.168.4.1"
 export ROSLAUNCH_SSH_UNKNOWN=1
 
 source /opt/ros/noetic/setup.bash
 source ~/quadrus_ws/devel/setup.bash
 
-exec "$@"
+exec "$@"-
