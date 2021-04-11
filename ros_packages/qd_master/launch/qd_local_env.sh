91ca5f660ba122ad67c2621e2c0f6123901baaca
olivierfournier2 <olivier.fournier@usherbrooke.ca>
2021-04-11 16:45:54 -0400

    launch debugging

--- ros_packages/qd_master/launch/qd_local_env.sh
+++ ros_packages/qd_master/launch/qd_local_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export ROS_MASTER_URI=http://localhost:11311/
-export ROS_IP="localhost"
+export ROS_MASTER_URI=http://192.168.4.17:11311/
+export ROS_IP="192.168.4.17"
 export ROSLAUNCH_SSH_UNKNOWN=1
 
 source /opt/ros/noetic/setup.bash
